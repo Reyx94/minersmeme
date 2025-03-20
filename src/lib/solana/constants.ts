2b42c93dec9718864216b10cb2d14738667cfb8d
Reyx94 <koenig94@icloud.com>
2025-03-20 03:17:19 +0100

    Update constants.ts

--- src/lib/solana/constants.ts
+++ src/lib/solana/constants.ts
@@ -17,4 +17,4 @@ export const AUTHORITY_REMOVAL_FEE_LAMPORTS = AUTHORITY_REMOVAL_FEE * LAMPORTS_P
 // Solana network configuration
 export const SOLANA_NETWORK = 'mainnet-beta'; // or 'mainnet-beta'
 //export const SOLANA_RPC_ENDPOINT = 'https://api.devnet.solana.com'; // Use this for devnet
- export const SOLANA_RPC_ENDPOINT = 'https://api.mainnet-beta.solana.com'; // Use this for mainnet
+ export const SOLANA_RPC_ENDPOINT = 'https://wandering-bitter-frog.solana-mainnet.quiknode.pro/5e3962e0ed51665ff1b11be64ba6b86c7360a711/'; // Use this for mainnet
